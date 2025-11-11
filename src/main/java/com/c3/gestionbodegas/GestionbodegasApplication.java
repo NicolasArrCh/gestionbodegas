033e89f6a14a86fbe982c719c4dd18eab24f2eb2
NicolasArrCh <nicolasarrubla115@gmail.com>
2025-11-11 14:43:48 -0500

    feat: :tada: Se inicia el proyecto de SpringBoot.

---
+++ src/main/java/com/c3/gestionbodegas/GestionbodegasApplication.java
@@ -0,0 +1,13 @@
+package com.c3.gestionbodegas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionbodegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionbodegasApplication.class, args);
+	}
+
+}
